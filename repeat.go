e869bd9c7a82826d53b5b5ca06a2ca6dd8502137
Raphael Costa <raphael.costa@m4u.com.br>
2020-03-18 02:47:40 -0300

    change business rules for Repeat function

--- repeat.go
+++ repeat.go
@@ -1,11 +1,9 @@
 package iteration
 
-const repeatCount = 5
-
 //Repeat repeats a string
-func Repeat(letter string) string {
+func Repeat(letter string, times int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < times; i++ {
 		repeated += letter
 	}
 	return repeated
