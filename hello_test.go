2666f1bd7b25f418ff946b4b1c636556cfbdb566
Raphael Costa <raphael.costa@m4u.com.br>
2020-03-17 23:42:17 -0300

    refactor assertion into a function

--- hello_test.go
+++ hello_test.go
@@ -4,21 +4,24 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
+	assertMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("Got %q want %q", got, want)
+		}
+	}
+
 	t.Run("should return Hello to a person", func(t *testing.T) {
 		got := Hello("Raphael")
 		want := "Hello Raphael"
 
-		if got != want {
-			t.Errorf("Got %q want %q", got, want)
-		}
+		assertMessage(t, got, want)
 	})
 
 	t.Run("should return Hello World when a empty string is passed as parameter", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello World"
 
-		if got != want {
-			t.Errorf("Got %q want %q", got, want)
-		}
+		assertMessage(t, got, want)
 	})
 }
