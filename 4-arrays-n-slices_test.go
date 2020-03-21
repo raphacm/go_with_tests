ae255e20e09deb4ea5b59b3dcae9fedeee7edf3e
Raphael Costa <raphael.costa@m4u.com.br>
2020-03-21 00:10:37 -0300

    create test for SumAll function

--- 4-arrays-n-slices_test.go
+++ 4-arrays-n-slices_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	assertEquals := func(t *testing.T, expected, received int) {
@@ -14,6 +17,23 @@ func TestSum(t *testing.T) {
 	t.Run("should return 16", func(t *testing.T) {
 		sum := Sum([]int{8, 8})
 		expected := 16
+
+		assertEquals(t, expected, sum)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	assertEquals := func(t *testing.T, expected, received []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(expected, received) {
+			t.Errorf("Expected %v but got %v", expected, received)
+		}
+	}
+
+	t.Run("should return an slice of sums other slices, in this case [20]", func(t *testing.T) {
+		sum := SumAll([]int{2, 2, 2, 2, 2}, []int{5, 4, 1})
+		expected := []int{10, 10}
 
 		assertEquals(t, expected, sum)
 	})
